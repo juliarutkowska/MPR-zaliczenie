1994360f19f2e634e1c0f40983dbd4d10b5cf15f
juliarutkowska <julia.rutkowska189@gmail.com>
2026-01-17 17:16:57 +0100

    Zmiana nazwy

--- src/main/java/pl/bank/s33840bank/S33840Bank.java
+++ src/main/java/pl/bank/s33840bank/S33840Bank.java
@@ -4,8 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class S33840BankApplication {
+public class S33840Bank {
 	public static void main(String[] args) {
-		SpringApplication.run(S33840BankApplication.class, args);
+		SpringApplication.run(S33840Bank.class, args);
 	}
 }
